dc75fa2c7270642efe2942057bc2c95f6c0f1d0b
Sal Tamay <sal.tamay@outlook.com>
2019-11-04 00:03:40 -0500

    feature: Can select the most important criteria

--- js/main.js
+++ js/main.js
@@ -113,7 +113,6 @@ function initMap() {
 }
 
 
-
 const handleFirstQuestion = (e) => {
   userInfo.numOfBeds = e.dataset.value;
   document.getElementById("mainContainer").innerHTML = `  
@@ -187,82 +186,6 @@ const handleFirstQuestion = (e) => {
   })
 };
 
-// const handleSecondQuestion = () => {
-
-//   userInfo.price = document.getElementById("sliderInput").value;
-
-//   const q3 = `  
-
-
-
-//   <header>
-//   <nav class="white center-align " role="navigation" style='height:5em'>
-//       <div class="nav-wrapper center-align">
-//           <a id="logo-container" href="#" class="brand-logo center-align" style="padding-top: 10px;">
-//               <img src="./img/isolated-monochrome-white.svg" alt="" srcset="" style="width: 30px;">
-//               <img class='hide-on-small-only' src="./img/default-monochrome-white.svg" alt="" srcset="" style="width: 96px; margin-left: 5px;">
-//           </a>
-//           <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-//       </div>
-//   </nav>
-// </header>
-  
-  
-  
-//   <div class="container " id='q3' style="margin-top:100px" >
-// <div class="row" >
-//     <div class="col s12 m10 offset-m1 l10 offset-l1 xl12 ">
-//       <div class="card red darken-4 z-depth-4">
-//         <div id='sliderCard' class="card-content white-text  ">
-//           <span class="card-title ">
-//             <i class='material-icons left green-text ' >my_location</i>
-//             Question 3 :
-//           </span>
-//           <p>WHERE ARE YOU LOOKING TO MOVE?</p>
-//         </div>
-//         <div class="container">
-//             <div class="row">
-//                 <form class="col s12 center-align">
-//                   <div class="row">
-//                     <div class="input-field col s12 center-align ">
-//                       <i class="material-icons prefix green-text">location_on</i>
-//                       <input id="location" type="text" class="validate white-text">
-//                       <label  class='green-text' for="location">Example: Toronto</label>
-//                     </div>
-//                   </div>
-//                   <a href="#" class='btn' onclick="handleThirdQuestion()">Submit 
-//                       <i class='material-icons right'>send</i>
-//                   </a>
-//                 </form>
-//               </div>
-//         </div>
-//     </div>
-//   </div>
-// </div>
-// </div>
-
-
-// <footer class="page-footer"  style='margin-top:100px; width:100vw; position:absolute; bottom:0px; '>
-// <div class="container center-align">
-//     <div class="col s12">
-//         <p class="grey-text text-lighten-4">We are a team of college students working on this project like
-//                 it's our full time job.
-//         </p>
-//     </div>
-// </div>
-
-//     <div class="footer-copyright">
-//       <div class="container center-align">
-//       &copy; 2019 Simply 
-//       </div>
-//     </div>
-// </footer>
-
-// `
-//   document.getElementById("mainContainer").innerHTML = q3;
-
-
-// };
 
 const handleSecondQuestion = () => {
   // userInfo.location = document.getElementById("location").value;
@@ -295,7 +218,7 @@ const handleSecondQuestion = () => {
           <p>I CARE THE MOST ABOUT:</p>
         </div>
         <div class="card-action center-align">
-          <a href="#" class='btn waves-effect waves-light z-depth-2' onclick="handleFourthQuestion(this)" data-value="transportaion">Easy Transportation</a>
+          <a href="#" class='btn waves-effect waves-light z-depth-2' onclick="handleFourthQuestion(this)" data-value="transportation">Easy Transportation</a>
           <p class='white-text' >OR</p>
           <a href="#" class='btn waves-effect waves-light z-depth-2' onclick="handleFourthQuestion(this)" data-value="price">Cheapest Price</a>
           <p class='white-text' >OR</p>
@@ -326,6 +249,7 @@ const handleSecondQuestion = () => {
   document.getElementById("mainContainer").innerHTML = q4;
 };
 
+
 const handleFourthQuestion = (e) => {
 
   userInfo.mostImportant = e.dataset.value;
@@ -339,17 +263,36 @@ const handleFourthQuestion = (e) => {
 
   listings = narrowByUnit();
 
-  // if (userInfo.mostImportant === 'price') {
+  if (userInfo.mostImportant === 'price') {
     
-  // }
-
-  listings = narrowByPrice(listings);
-
-  narrowByWalkingDistance(listings);
+    return narrowByPrice(listings).sort((a, b) => {
+      return b.price - a.price;
+    })
+  } 
   
-  // displayresults()
+  if (userInfo.mostImportant === 'transportation') {
+
+    return narrowByWalkingDistance(listings).sort((a, b) => {
+      return a.distance - b.distance;
+    });
+  }
+
+  if (userInfo.mostImportant === 'both') {
+    // const newListing = [];
+
+    console.log(narrowByWalkingDistance(listings).filter(listing => {
+
+      if (listing.listing.price <= parseInt(userInfo.price)) {
+        return listing;
+      }
+    }))
+    
+  }
+  
+  displayresults()
 
 }
+
 
 const displayresults = function () {
   convertLatLong()
@@ -379,6 +322,7 @@ const saveToLocalStorage = () => {
   localStorage.setItem('userInfo', JSON.stringify(userInfo));
 
 }
+
 
 const displayFirstQuestion = () => {
 
@@ -450,16 +394,18 @@ const narrowByPrice = (listings) => {
 
   listings.forEach(listing => {
     
-    let price = listing.price.slice(2, 3) + listing.price.slice(4, 7);
-    price = parseInt(price);
+    // let price = listing.price.slice(2, 3) + listing.price.slice(4, 7);
+    // price = parseInt(price);
     
-    if (price <= parseInt(userInfo.price)) {
+    if (listing.price <= parseInt(userInfo.price)) {
+      // listing.price = price;
       newListings.push(listing);
     }
   });
   console.log(newListings);
   return newListings;
 }
+
 
 const narrowByUnit = () => {
   
@@ -471,6 +417,11 @@ const narrowByUnit = () => {
     
     if (listing.hdpData) {
       if (listing.beds.toString() === userInfo.numOfBeds) {
+        
+        let price = listing.price.slice(2, 3) + listing.price.slice(4, 7);
+        price = parseInt(price);
+        listing.price = price;
+
         newListings.push(listing);
       }
     }
@@ -478,59 +429,85 @@ const narrowByUnit = () => {
   return newListings;
 }
 
+
 const narrowByWalkingDistance = (listings) => {
 
+  // return new Promise((resolve, reject) => {
+  //   const newListings = [];
+
+  //   listings.forEach(listing => {
+
+  //     const waypoint0 = listing.latLong.latitude + ',' + listing.latLong.longitude;
+
+  //     subwayData.forEach(subwayLocation => {
+
+  //       const waypoint1 = subwayLocation.latLong.latitude + ',' + subwayLocation.latLong.longitude;
+
+  //       $.ajax({
+  //         url: 'https://route.api.here.com/routing/7.2/calculateroute.json',
+  //         type: 'GET',
+  //         dataType: 'jsonp',
+  //         jsonp: 'jsoncallback',
+  //         data: {
+  //           waypoint0: waypoint0,
+  //           waypoint1: waypoint1,
+  //           mode: 'fastest;pedestrian',
+  //           app_id: 'JAsUvJIQP95l3YaTmtT9',
+  //           app_code: 'm7Ujzi9liyMLwQ27-0IE1Q'
+  //         },
+  //       }).then(data => {
+  //         const distance = data.response.route[0].summary.distance;
+  //           console.log('call');
+  //           if (distance <= 750) {
+  //             newListings.push({
+  //               listing: listing,
+  //               subway: subwayLocation,
+  //               distance: distance
+  //             });
+  //           }
+  //       })
+  //     })
+  //   });
+  //   // return newListings;
+  //   if(newListings.length === newListings.length) {
+  //     resolve(newListings);
+  //   }else {
+  //     reject('Cannot complete');
+  //   }
+  // })
   const newListings = [];
+  const r = 6371e3; // gives d in metres
+  for (unit in listings) {
+    var φ1 = (listings[unit].latLong.latitude) * Math.PI / 180;
+    for (station in subwayData) {
+      var φ2 = (subwayData[station].latLong.latitude) * Math.PI / 180;
+      var Δλ = ((subwayData[station].latLong.longitude - listings[unit].latLong.longitude)) * Math.PI / 180;
+      var d = Math.acos(Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)) * r;
 
-  listings.forEach(listing => {
-    
-    const waypoint0 = listing.latLong.latitude + ',' + listing.latLong.longitude;
-
-    subwayData.forEach(subwayLocation => {
-
-      const waypoint1 = subwayLocation.latLong.latitude + ',' + subwayLocation.latLong.longitude;
-      
-      $.ajax({
-        url: 'https://route.api.here.com/routing/7.2/calculateroute.json',
-        type: 'GET',
-        dataType: 'jsonp',
-        jsonp: 'jsoncallback',
-        data: {
-          waypoint0: waypoint0,
-          waypoint1: waypoint1,
-          mode: 'fastest;pedestrian',
-          app_id: 'JAsUvJIQP95l3YaTmtT9',
-          app_code: 'm7Ujzi9liyMLwQ27-0IE1Q'
-        },
-        success: function (data) {
-          const distance = data.response.route[0].summary.distance;
-          if (distance <= 750) {
-            newListings.push({
-              listing: listing,
-              subway: subwayLocation,
-              distance: distance
-            });
-          }
-        }
-      });
-    })
-  });
+      if (d <= 750) {
+        // console.log('distance from unit at', listings[unit].latLong, 'to', subwayData[station].name, 'station is', Math.floor(d), 'meters')
+        newListings.push({
+          listing: listings[unit],
+          subway: subwayData[station],
+          distance: Math.floor(d)
+        });
+      }
+    }
+  }
   console.log(newListings);
   return newListings;
 }
+
 
 const getListing = async function(){
   localStorage.setItem('houseListing', JSON.stringify(listingsInfo.searchResults.mapResults));
 }
 
 
-
-
-
-const main = async function () {
+const main = function () {
   localStorage.clear(); //clears the local storgage
 
-  await getListing()
+  getListing()
 
   document.querySelector('#getStarted').addEventListener('click', (e) => {
     e.preventDefault();
